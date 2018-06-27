954f93a19ce6045cdcbc0c64b45c22a34d3a5bf5
Colben Kharrl <colbenkharrl@gmail.com>
2018-06-26 19:31:02 -0700

    creation of band membership relationships when creating a new band

--- php/add_band.php
+++ php/add_band.php
@@ -23,12 +23,30 @@ $end_date = date('Y-m-d', strtotime($_POST['end_date']));
 
 $sql = "INSERT INTO BAND (BandID, bandName, genre, startDate, endDate) VALUES (null,'$band_name', '$genre', '$start_date','$end_date')";
 if(mysqli_query($connection,$sql)){
-  echo "Records added Successfully.";
+  echo "Band added Successfully.";
   echo "<br />";
-  echo "<a class='btn btn-success' href='../list-bands.html'>Return Home</a>";
+
+  $band_id = mysqli_insert_id($connection);
+
+  $members = array();
+  foreach( $_POST['members'] as $musician_id ) {
+	  $members[] = '('.$musician_id.', '.$band_id.', "unimplemented", "2018-06-26", "2018-06-26")';
+  }
+
+  $members_query = 'INSERT INTO MEMBER_OF (MusicianID, BandID, role, startDate, endDate) VALUES '.implode(',', $members).';';
+
+  if(mysqli_query($connection,$members_query)){
+  echo "Tracklist added Successfully.";
+  echo "<br />";
+  } else {
+  echo "ERROR: Cound not execute sql." . mysqli_error($connection);
+  }
 } else {
   echo "ERROR: Cound not execute sql." . mysqli_error($connection);
 }
+
+echo "<a class='btn btn-success' href='../list-bands.html'>Return Home</a>";
+
 mysqli_close($connection);
 
  ?>
